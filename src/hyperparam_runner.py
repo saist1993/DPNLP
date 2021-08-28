69e6ef6552282da50d579cd1b0e1691bcfd43c37
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-08-28 14:37:16 +0200

    added leakage calculation into the mix.

--- src/hyperparam_runner.py
+++ src/hyperparam_runner.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     else:
         fairness_score_function = 'multiple_things'
 
-    if dataset_name == 'encoded_emoji' or 'blog' in dataset_name:
+    if dataset_name == 'encoded_emoji' or 'blog' in dataset_name or "encoded_bias_in_bios" == dataset_name:
         calculate_leakage = True
     else:
         calculate_leakage = False
