7118d5b321b1342cef3377ecc3c720815ebe6d02
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-10-07 12:00:36 +0200

    more deeper model

--- src/models/linear.py
+++ src/models/linear.py
@@ -260,32 +260,60 @@ class SimpleNonLinear(nn.Module):
         input_dim = params['model_arch']['encoder']['input_dim']
         output_dim = params['model_arch']['encoder']['output_dim']
 
-        self.layer_1 = nn.Linear(input_dim, 128)
-        self.layer_2 = nn.Linear(128, 64)
-        self.layer_3 = nn.Linear(64, 32)
+        self.layer_1 = nn.Linear(input_dim, 512)
+        self.layer_2 = nn.Linear(512, 256)
+        self.layer_3 = nn.Linear(256, 128)
+        self.layer_4 = nn.Linear(128, 128)
+        self.layer_5 = nn.Linear(128, 64)
+        self.layer_6 = nn.Linear(64, 32)
         self.layer_out = nn.Linear(32, output_dim)
 
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.2)
-        self.batchnorm1 = nn.BatchNorm1d(128)
-        self.batchnorm2 = nn.BatchNorm1d(64)
-        self.batchnorm3 = nn.BatchNorm1d(32)
+        self.batchnorm1 = nn.BatchNorm1d(512)
+        self.batchnorm2 = nn.BatchNorm1d(256)
+        self.batchnorm3 = nn.BatchNorm1d(128)
+        self.batchnorm4 = nn.BatchNorm1d(128)
+        self.batchnorm5 = nn.BatchNorm1d(64)
+        self.batchnorm6 = nn.BatchNorm1d(32)
 
     def forward(self, params):
         x = params['input']
+
+
         x = self.layer_1(x)
-        # x = self.batchnorm1(x)
+        x = self.batchnorm1(x)
         x = self.relu(x)
 
         x = self.layer_2(x)
-        # x = self.batchnorm2(x)
+        x = self.batchnorm2(x)
         x = self.relu(x)
-        x = self.dropout(x)
 
         x = self.layer_3(x)
-        # x = self.batchnorm3(x)
+        x = self.batchnorm3(x)
         x = self.relu(x)
-        x = self.dropout(x)
+
+        x = self.layer_4(x)
+        x = self.batchnorm4(x)
+        x = self.relu(x)
+
+        x = self.layer_5(x)
+        x = self.batchnorm5(x)
+        x = self.relu(x)
+
+        x = self.layer_6(x)
+        x = self.batchnorm6(x)
+        x = self.relu(x)
+
+        # x = self.layer_2(x)
+        # # x = self.batchnorm2(x)
+        # x = self.relu(x)
+        # x = self.dropout(x)
+        #
+        # x = self.layer_3(x)
+        # # x = self.batchnorm3(x)
+        # x = self.relu(x)
+        # x = self.dropout(x)
 
         x = self.layer_out(x)
 
