81ff4cc1288f733af61cab0dddfe65ae743fa930
gmaheshwari <gaurav.maheshwari@inria.fr>
2021-10-08 14:24:24 +0200

    norm is now abs.

--- src/training_loops/fair_grad.py
+++ src/training_loops/fair_grad.py
@@ -125,7 +125,8 @@ def train(model, iterator, optimizer, criterion, device, accuracy_calculation_fu
 
             if other_params['normalize_fairness']:
                 normalization = torch.mean(1.0-fairness)
-                assert normalization > 0.0
+                normalization = abs(normalization)
+                # assert normalization > 0.0
             else:
                 normalization = torch.tensor(1.0)
 
